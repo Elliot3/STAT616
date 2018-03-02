e978568ea85bc3986dc805668e26b34fec97a2d5
Elliot Smith <esmith2642@gmail.com>
2018-03-01 20:31:29 -0600

    hw3 prob1

--- HW03/HW03.R
+++ HW03/HW03.R
@@ -1,3 +1,7 @@
+library(pairwiseCI)
+
+
+
 ########## Problem 1
 
 
@@ -37,13 +41,122 @@ bartlett.test(value ~ type, data = data_1)
 
 ##### Part b
 
+
+
+### Standard Confidence Intervals
+
+### 1A - Confidence Interval
+
+data_1A <- data_1[1:10,]
+n <- length(data_1A$value)
+mean_1A <- mean(data_1A$value)
+sd_1A <- sd(data_1A$value)
+
+error_1A <- qt(0.975, df = n - 1)*((sd_1A)/sqrt(n))
+
+left_1A <- mean_1A - error_1A
+right_1A <- mean_1A + error_1A
+
+### 1B - Confidence Interval
+
+data_1B <- data_1[11:20,]
+n <- length(data_1B$value)
+mean_1B <- mean(data_1B$value)
+sd_1B <- sd(data_1B$value)
+
+error_1B <- qt(0.975, df = n - 1)*((sd_1B)/sqrt(n))
+
+left_1B <- mean_1B - error_1B
+right_1B <- mean_1B + error_1B
+
+### 1C - Confidence Interval
+
+data_1C <- data_1[21:30,]
+n <- length(data_1C$value)
+mean_1C <- mean(data_1C$value)
+sd_1C <- sd(data_1C$value)
+
+error_1C <- qt(0.975, df = n - 1)*((sd_1C)/sqrt(n))
+
+left_1C <- mean_1C - error_1C
+right_1C <- mean_1C + error_1C
+
+### 1D - Confidence Interval
+
+data_1D <- data_1[31:40,]
+n <- length(data_1D$value)
+mean_1D <- mean(data_1D$value)
+sd_1D <- sd(data_1D$value)
+
+error_1D <- qt(0.975, df = n - 1)*((sd_1D)/sqrt(n))
+
+left_1D <- mean_1D - error_1D
+right_1D <- mean_1D + error_1D
+
+### 1E - Confidence Interval
+
+data_1E <- data_1[41:50,]
+n <- length(data_1E$value)
+mean_1E <- mean(data_1E$value)
+sd_1E <- sd(data_1E$value)
+
+error_1E <- qt(0.975, df = n - 1)*((sd_1E)/sqrt(n))
+
+left_1E <- mean_1E - error_1E
+right_1E <- mean_1E + error_1E
+
+
+
+### Bonferroni Confidence Intervals
+
+### 1A - Confidence Interval
+
+error_1A_bon <- qt(0.995, df = n - 1)*((sd_1A)/sqrt(n))
+
+left_1A_bon <- mean_1A - error_1A_bon
+right_1A_bon <- mean_1A + error_1A_bon
+
+### 1B - Confidence Interval
+
+error_1B_bon <- qt(0.995, df = n - 1)*((sd_1B)/sqrt(n))
+
+left_1B_bon <- mean_1B - error_1B_bon
+right_1B_bon <- mean_1B + error_1B_bon
+
+### 1C - Confidence Interval
+
+error_1C_bon <- qt(0.995, df = n - 1)*((sd_1C)/sqrt(n))
+
+left_1C_bon <- mean_1C - error_1C_bon
+right_1C_bon <- mean_1C + error_1C_bon
+
+### 1D - Confidence Interval
+
+error_1D_bon <- qt(0.995, df = n - 1)*((sd_1D)/sqrt(n))
+
+left_1D_bon <- mean_1D - error_1D_bon
+right_1D_bon <- mean_1D + error_1D_bon
+
+### 1E - Confidence Interval
+
+error_1E_bon <- qt(0.995, df = n - 1)*((sd_1E)/sqrt(n))
+
+left_1E_bon <- mean_1E - error_1E_bon
+right_1E_bon <- mean_1E + error_1E_bon
+
+# The Bonferroni confidence intervals are much more conservative than our traditional t-statistic confidence intervals (ie its a wider range). I prefer
+# the traditional confidence interval due to the fact that with Bonferroni we are less likely to reject Ho which means we would be more likely to commit
+# a Type II error.
+
+
+
+##### Part c
+
+
+
 ### Construct the ANOVA model
 
 anova_1 <- aov(value ~ type, data = data_1)
-
-### Calculate the mean values per factor level
-
-mean_vals_1 <- tapply(data_1$value, data_1$type, mean)
 
 ### Construct the Tukey confidence intervals
 
@@ -51,13 +164,11 @@ TukeyHSD(anova_1)
 
 ### Construct the Bonferroni confidence intervals
 
-confint(anova_1)
+pairwiseCI(value ~ type, data = data_1, conf.level = 0.99)
 
-
-
-##### Part c
-
-# TBD
+# The Bonferroni confidence intervals are much more conservative than our traditional t-statistic confidence intervals (ie its a wider range). I prefer
+# the traditional confidence interval due to the fact that with Bonferroni we are less likely to reject Ho which means we would be more likely to commit
+# a Type II error.
 
 
 
